2e1928c21f892d48791ab32cbb6bd665a14dcec4
Cian Keegan <161904946+ckeegan12@users.noreply.github.com>
2025-04-27 18:54:40 -0400

    Create Flattening.m

---
+++ lib/Flattening.m
@@ -0,0 +1,8 @@
+function output = Flattening(Input)
+  % input: 4D tensor with size [batch_size, height, width, Pooling_Layers]
+  % output: 1D array of Average_Pooling values after applying the Flattening function
+
+  % Parameters
+  [batch_size, height, width, feature_maps] = size(input);  % Get input size
+
+end
